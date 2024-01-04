18b5da4c72085f98c9a49b37a33835c947d8e38f
23A91A1209 <147414489+23A91A1209@users.noreply.github.com>
2024-01-04 11:15:27 -0500

    #include<stdio.h> int main() {     int a,b,i,j;     scanf("%d %d", &a,&b);     int mat1[a][b];     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             scanf("%d", &mat1[i][j]);         }     }     int c,d;     scanf("%d %d", &c,&d);     int mat2[c][d];     for(i=0;i<c;i++)     {         for(j=0;j<d;j++)         {             scanf("%d", &mat2[i][j]);         }     }     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             mat1[i][j]+=mat2[i][j];         }     }     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             printf("%d ", mat1[i][j]);         }         printf(" ");     } }

---
+++ Matrix_Addition.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j;
+    scanf("%d %d", &a,&b);
+    int mat1[a][b];
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+    int c,d;
+    scanf("%d %d", &c,&d);
+    int mat2[c][d];
+    for(i=0;i<c;i++)
+    {
+        for(j=0;j<d;j++)
+        {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            mat1[i][j]+=mat2[i][j];
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            printf("%d ", mat1[i][j]);
+        }
+        printf("
+");
+    }
+}
