a46a58994e217c9db1caa98df0cb117676172e3e
23A91A1209 <147414489+23A91A1209@users.noreply.github.com>
2024-03-19 11:00:32 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d", &n);     int arr[n];     int flag=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==0)         {             flag=1;             break;         }     }     if(flag)     {         printf("%d", i);     } }

---
+++ Last_even_element_index.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d", &n);
+    int arr[n];
+    int flag=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag)
+    {
+        printf("%d", i);
+    }
+}
